d5972c08ab3b8b3d7030654563240900324d7f2d
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2020-05-14 13:39:48 +0200

    separate filters from generator

--- filters.py
+++ filters.py
@@ -1,14 +1,44 @@
 import numpy as np
+from typing import Tuple
 
-def clip(a: np.array, threshold: float, both: bool = True) -> np.array:
+
+def clip(a: np.array, threshold: float, mix: float = 1) -> np.array:
     max = np.max(a)
-    a = a / max
-    a = np.where(a < threshold, a, threshold)
-    if both:
-        a = np.where(a > -threshold, a, -threshold)
-    clipped = a * max
-    return clipped
+    if max == 0:
+        # a is a zero array
+        return a
+    c = a / max
+    c = np.where(a < threshold, a, threshold)
+    c = np.where(c > -threshold, c, -threshold)
+    c = c * max
+    return mix * c + (1 - mix) * a
 
 
 def normalize(a: np.array) -> np.array:
     return a / np.max(a)
+
+
+def adsr(
+    s: np.array,
+    attack: Tuple[float, float],
+    decay: Tuple[float, float],
+    release: Tuple[float, float],
+    mix: float = 1,
+) -> np.array:
+    """This isn't reeeeally an ADSR filter, but it's inspired by it."""
+    length = len(s)
+    (att_l, att_n) = (attack[0], int(length * attack[1]))
+    (dec_l, dec_n) = (decay[0], int(length * decay[1]))
+    (rel_l, rel_n) = (release[0], int(length * release[1]))
+    sus_n = length - (att_n + dec_n + rel_n)
+
+    att = np.linspace(0, att_l, att_n)
+    dec = np.linspace(att_l, dec_l, dec_n)
+    sus = np.linspace(dec_l, dec_l, sus_n)
+    rel = np.linspace(dec_l, rel_l, rel_n)
+
+    adsr = np.concatenate([att, dec, sus, rel])
+    assert len(adsr) == length
+
+    f = np.multiply(s, adsr)
+    return mix * f + (1 - mix) * s
--- quothe.py
+++ quothe.py
@@ -7,18 +7,16 @@ class Generator:
     def __init__(self, fn: Callable = np.sin, rate: int = 44100):
         self.rate = rate
         self.fn = fn
-        self.attack = (1, 1/32)
-        self.decay = (3/4, 1/8)
-        self.release = (0, 1/64)
+        self.filters = []
 
     def signal(self, frequency: float, fn: Callable = None, duration: float = 1):
         fn = fn or self.fn
         n = int(duration * self.rate)
         t = np.linspace(0, 1 * duration, int(self.rate * duration))
         s = fn(t * 2 * np.pi * frequency)
-        filter = self._adsr(n, attack=self.attack, decay=self.decay, release=self.release)
-        filtered = np.multiply(s, filter)
-        return filtered
+        for f in self.filters:
+            s = f(s)
+        return s
 
     def chord(self, notes: List[str], fn: Callable = None, duration: float = 1):
         fn = fn or self.fn
@@ -37,29 +35,4 @@ class Generator:
         notes = [change_to_chord(n) for n in notes]
         melody = np.concatenate([self.chord(n, duration=d) for n, d in notes])
         return melody
-
-    def _trapezoid(self, length: int):
-        slope = int(length / 16)
-        rise = np.linspace(0, 1, slope)
-        flat = np.ones(length - 2 * slope)
-        fall = np.linspace(1, 0, slope)
-        return np.concatenate([rise, flat, fall])
-
-    def _adsr(
-            self, length: int, attack: Tuple[float, float], decay: Tuple[float, float], release: Tuple[float, float]
-    ):
-        """This isn't reeeeally an ADSR filter, but it's inspired by it."""
-        (att_l, att_n) = (attack[0], int(length*attack[1]))
-        (dec_l, dec_n) = (decay[0], int(length*decay[1]))
-        (rel_l, rel_n) = (release[0], int(length*release[1]))
-        sus_n = length - (att_n + dec_n + rel_n)
-
-        att = np.linspace(0, att_l, att_n)
-        dec = np.linspace(att_l, dec_l, dec_n)
-        sus = np.linspace(dec_l, dec_l, sus_n)
-        rel = np.linspace(dec_l, rel_l, rel_n)
-
-        adsr = np.concatenate([att, dec, sus, rel])
-        assert len(adsr) == length
-        return adsr
 
